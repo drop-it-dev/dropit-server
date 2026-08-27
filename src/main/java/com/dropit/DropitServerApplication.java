4b91282f12029862b8657da6adcc3e19fb6aabfb
Sooyoung Yoon <ayoooo240@gmail.com>
2026-08-27 13:10:41 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/dropit/DropitServerApplication.java
@@ -0,0 +1,13 @@
+package com.dropit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropitServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DropitServerApplication.class, args);
+    }
+
+}
