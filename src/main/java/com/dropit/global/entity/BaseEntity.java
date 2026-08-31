f0d1b6f3cab2116c233d05ca652f2a208b1f6fd6
Hyiyek <hy0013@naver.com>
2026-08-31 13:54:15 +0900

    fix : BaseEntity 수정

--- src/main/java/com/dropit/global/entity/BaseEntity.java
+++ src/main/java/com/dropit/global/entity/BaseEntity.java
@@ -3,8 +3,6 @@ package com.dropit.global.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -23,16 +21,4 @@ public abstract class BaseEntity {
 
     @LastModifiedDate
     private LocalDateTime updatedAt;
-
-    @PrePersist
-    protected void onCreate() {
-        LocalDateTime now = LocalDateTime.now();
-        this.createdAt = now;
-        this.updatedAt = now;
-    }
-
-    @PreUpdate
-    protected void onUpdate() {
-        this.updatedAt = LocalDateTime.now();
-    }
 }
