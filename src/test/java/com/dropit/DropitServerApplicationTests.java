06add6ce2ad6caa44bf705b5b1dfd9f7075d5887
Sooyoung Yoon <ayoooo240@gmail.com>
2026-09-07 15:28:54 +0900

    ci(workflow): CI 워크플로우 생성, actuator, parameter store 의존성 추가

--- src/test/java/com/dropit/DropitServerApplicationTests.java
+++ src/test/java/com/dropit/DropitServerApplicationTests.java
@@ -1,13 +1,7 @@
 package com.dropit;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DropitServerApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
